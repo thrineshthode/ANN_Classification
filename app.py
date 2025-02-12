c12d2bf85d9ade4522f147825554ba8ea3ec3848
thrineshthode <113592959+thrineshthode@users.noreply.github.com>
2025-02-12 17:45:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def main():
     gender=st.selectbox('Gender',label_encoder_gender.classes_)
     age=st.slider('Age',18,92)
     balance=st.number_input('Balance')
-    credit_score= st.number_input('Credit Score')
+    credit_score= st.number_input('Credit Score',max_value=1000)
     estimated_salary=st.number_input('Estimated Salary')
     tenure= st.slider('Tenure',0,10)
     num_of_products=st.slider('Number of Products',1,4)
